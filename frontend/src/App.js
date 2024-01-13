d0b4c0b6008b7e3132d411038f5242da77ab9835
Abhinav Singh <abhinavas430@gmail.com>
2024-01-13 15:19:17 +0530

    Trying to fix cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@ import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("https://video-calling-app-beige.vercel.app/");
+const socket = io.connect("https://video-calling-app-beige.vercel.app/", {
+  reconnection: true,
+  reconnectionAttempts: 3,
+});
 
 function App() {
   const [me, setMe] = useState("");
